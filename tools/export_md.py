c1e4863cbdffeaabfc231b77aa24f43b185ccbc6
Mohammed Amer <mohammed.e.amer@gmail.com>
2025-10-05 15:35:39 +0100

    relative asset urls

--- tools/export_md.py
+++ tools/export_md.py
@@ -3,6 +3,7 @@ import subprocess
 from pathlib import Path
 import os
 import shutil
+import re
 
 
 THIS = Path(os.path.dirname(os.path.realpath(__file__)))
@@ -17,11 +18,28 @@ def run(article, year, month, day):
                    "markdown", article/"main.ipynb"])
 
     article_tag = article.name
-    post_name = f"{year}-{month}-{day}-{article_tag}"
-    shutil.move(article/"main.md", POSTS/f"{post_name}.md")
+    article_path = article/"main.md"
 
-    shutil.copytree(article/f"{article_tag}", ROOT /
-                    "assets/img"/f"{article_tag}")
+    post_name = f"{year}-{month}-{day}-{article_tag}"
+
+    with open(article_path, "r") as f:
+        text = f.read()
+
+    matches = re.findall(
+        r'<img\b[^>]*\bsrc\s*=\s*(["\'])([^"\']+)\1', text, flags=re.I)
+    for m in matches:
+        relative = m[1].replace("assets", f"/assets/{post_name}")
+        relative = f"{{{{ '{relative}' | relative_url }}}}"
+        text = text.replace(m[1], relative)
+
+    with open(article_path, "w") as f:
+        f.write(text)
+
+    post_path = POSTS/f"{post_name}.md"
+    shutil.move(article/"main.md", post_path)
+
+    shutil.copytree(article/"assets", ROOT /
+                    f"assets/{post_name}")
 
     print("Done!")
 
